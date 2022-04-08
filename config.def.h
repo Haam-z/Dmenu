846fb6f77fbf526d824e3adc8de6d3d801af1508
Haam <hamzazarouk@gmail.com>
2022-04-08 11:14:28 +0100

    added mytheme patch

--- config.def.h
+++ config.def.h
@@ -9,10 +9,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#d1d1d1", "#01060b" },
+	[SchemeSel] = { "#1d1c21", "#89dceb" },
+	[SchemeSelHighlight] = { "#1d1c21", "#89dceb" },
+	[SchemeNormHighlight] = { "#abe9b3", "#01060b" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
